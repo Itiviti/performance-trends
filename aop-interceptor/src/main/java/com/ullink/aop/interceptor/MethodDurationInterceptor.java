849783354050417e929d14157da5b42d8fcc020a
Laszlo Miklosik <laszlo.miklosik@ullink.com>
2014-11-25 10:00:12 +0200

    Switched to logging directly with IndexedChronicle
    Instead of slf4j facade (which caused version incompatibility issues with some Ullink products).
    Less dependencies in final agent fatjar -> more chances to be compatible with projects (no LinkageErrors).

--- aop-interceptor/src/main/java/com/ullink/aop/interceptor/MethodDurationInterceptor.java
+++ aop-interceptor/src/main/java/com/ullink/aop/interceptor/MethodDurationInterceptor.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class MethodDurationInterceptor implements MethodInterceptor {
 
-    private static final String LOG_MESSAGE_FORMAT = "## Execution took %d nanos for %s.%s using arguments %s";
+    private static final String LOG_MESSAGE_FORMAT = "Execution took %d nanos for %s.%s using arguments %s";
     private static final FastLogger LOGGER = FastLogger.getInstance();
 
     @Override
@@ -20,7 +20,7 @@ public class MethodDurationInterceptor implements MethodInterceptor {
         } finally {
             long durationNanos = System.nanoTime() - startNanoSeconds;
             Method method = invocation.getMethod();
-            String className = method.getDeclaringClass().getSimpleName();
+            String className = method.getDeclaringClass().getName();
             String methodName = method.getName();
             String methodArguments = Arrays.toString(invocation.getArguments());
             String logMessage = String.format(LOG_MESSAGE_FORMAT, durationNanos, className, methodName, methodArguments);
--- fast-logger/src/main/java/com/ullink/duration/logging/FastLogger.java
+++ fast-logger/src/main/java/com/ullink/duration/logging/FastLogger.java
@@ -1,25 +1,59 @@
 package com.ullink.duration.logging;
 
-import net.openhft.chronicle.logger.slf4j.ChronicleLoggingConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.openhft.chronicle.Chronicle;
+import net.openhft.chronicle.ExcerptAppender;
+import net.openhft.chronicle.IndexedChronicle;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class FastLogger
 {
-    static
-    {
-        if (System.getProperty(ChronicleLoggingConfig.KEY_PROPERTIES_FILE) == null)
-        {
-            System.setProperty(ChronicleLoggingConfig.KEY_PROPERTIES_FILE, "duration-logging.properties");
-        }
-    }
 
-    private static Logger LOGGER = LoggerFactory.getLogger("DURATION");
-
-    private static FastLogger instance;
+    private static final int              EXCERPT_LINE_CAPACITY_IN_BYTES = 512;
+    private static final SimpleDateFormat LOG_DATETIME_FORMATTER         = new SimpleDateFormat("yyyy-MM-DD HH:mm:ss.SSS");
+    private static final SimpleDateFormat FILENAME_DATE_FORMATTER        = new SimpleDateFormat("yyyy-MM-DD");
+    public static final  String           LOG_FOLDER_NAME                = "durations";
+    public static final String LOGFILE_PREFIX = "duration-";
+    private static FastLogger      instance;
+    private        Chronicle       chronicle;
+    private        ExcerptAppender logger;
 
     private FastLogger()
     {
+        try
+        {
+            // TODO: make path configurable, only use this as default
+            final String logBaseDir = System.getProperty("java.io.tmpdir");
+            final String fileName = LOGFILE_PREFIX + FILENAME_DATE_FORMATTER.format(System.currentTimeMillis());
+            String basePath = logBaseDir + LOG_FOLDER_NAME + File.separator + fileName;
+            System.out.println("Base path is: " + basePath);
+            chronicle = new IndexedChronicle(basePath);
+            logger = chronicle.createAppender();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error creating Chronicle logger: " + e);
+        }
+
+        closeChronicle();
+    }
+
+    private void closeChronicle()
+    {
+        Runtime currentRuntime = Runtime.getRuntime();
+        currentRuntime.addShutdownHook(
+            new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    System.out.println("Shutting down logger");
+                    logger.close();
+                }
+            }
+        );
     }
 
     public static FastLogger getInstance()
@@ -33,6 +67,11 @@ public class FastLogger
 
     public void log(final String logMessage)
     {
-        LOGGER.info(logMessage);
+        logger.startExcerpt(EXCERPT_LINE_CAPACITY_IN_BYTES);
+        logger.append(LOG_DATETIME_FORMATTER.format(System.currentTimeMillis()));
+        logger.append(" ");
+        logger.append(logMessage);
+        logger.append('\n');
+        logger.finish();
     }
 }
