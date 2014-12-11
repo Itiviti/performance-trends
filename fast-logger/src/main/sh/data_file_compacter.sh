cbc98fcc68a1fa11bde7403f1e209e7ba41dd138
Laszlo Miklosik <laszlo.miklosik@ullink.com>
2014-12-11 11:53:56 +0200

    Small update to data/log file compacter shell script

--- fast-logger/src/main/sh/data_file_compacter.sh
+++ fast-logger/src/main/sh/data_file_compacter.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 INPUT_DIR=/data/logs/durations
 OUTPUT_DIR=/apps/performance-trends-visualizer/loginput
+ORIGINAL_FILE_EXTENSION='*.data'
 
-rm -rf *.index
-outputFileName=merged-`date --iso-8601=seconds`.log
-find $INPUT_DIR -type f -name '*.data' -exec grep -a '`' {} \; > $INPUT_DIR/$outputFileName
+numberOfInputFiles=`find $INPUT_DIR -type f -name $ORIGINAL_FILE_EXTENSION |wc -l`
 
-echo 'Output file ready, it start like this: '
-head $INPUT_DIR/$outputFileName
-echo 'Ends like this: '
-tail $INPUT_DIR/$outputFileName
+if [ $numberOfInputFiles -gt 0 ]; then
+  START_TIME=$SECONDS
+  echo "Found  $numberOfInputFiles files matching $ORIGINAL_FILE_EXTENSION to merge! Now starting to merge."
+  rm -rf $INPUT_DIR/*.index
+  outputFileName=merged-`date --iso-8601=seconds`.log
+  find $INPUT_DIR -type f -name $ORIGINAL_FILE_EXTENSION -exec grep -a '`' {} \; > $INPUT_DIR/$outputFileName
 
-echo "Moving file to logstah input folder: $OUTPUT_DIR/$outputFileName"
+  echo "Output file is ready and it starts like this:"
+  head -n 1 $INPUT_DIR/$outputFileName
+  echo "And it ends like this:"
+  tail -n 1 $INPUT_DIR/$outputFileName
 
-rm -rf *.data
-mv $INPUT_DIR/$outputFileName  $OUTPUT_DIR/$outputFileName
+  echo "Moving file to logstah input folder: $OUTPUT_DIR/$outputFileName"
+
+  rm -rf $INPUT_DIR/$ORIGINAL_FILE_EXTENSION
+  mv $INPUT_DIR/$outputFileName  $OUTPUT_DIR/$outputFileName
+  ELAPSED_TIME=$(($SECONDS - $START_TIME))
+  echo "Compating files took: $ELAPSED_TIME seconds"
+
+else
+  echo "No $ORIGINAL_FILE_EXTENSION files found to merge!"
+fi
