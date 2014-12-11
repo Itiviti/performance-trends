85d317c52f6452fcc6fc1486bd595f68761a4173
Laszlo Miklosik <laszlo.miklosik@ullink.com>
2014-12-11 12:24:58 +0200

    Made compacter shell script windows compatible (can now be run from Git bash)

--- fast-logger/src/main/sh/data_file_compacter.sh
+++ fast-logger/src/main/sh/data_file_compacter.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
+
+# Linux paths
 INPUT_DIR=/data/logs/durations
 OUTPUT_DIR=/apps/performance-trends-visualizer/loginput
-ORIGINAL_FILE_EXTENSION='*.data'
 
+# Windows compatible paths (to make the script work in Git bash)
+#INPUT_DIR=/D/logs/durations
+#OUTPUT_DIR=/C/performance-trends-visualizer/loginput
+
+ORIGINAL_FILE_EXTENSION='*.data'
 numberOfInputFiles=`find $INPUT_DIR -type f -name $ORIGINAL_FILE_EXTENSION |wc -l`
 
 if [ $numberOfInputFiles -gt 0 ]; then
   START_TIME=$SECONDS
   echo "Found  $numberOfInputFiles files matching $ORIGINAL_FILE_EXTENSION to merge! Now starting to merge."
   rm -rf $INPUT_DIR/*.index
-  outputFileName=merged-`date --iso-8601=seconds`.log
+  outputFileName=merged-`date +%Y-%m-%d-%H%M%S`.log
   find $INPUT_DIR -type f -name $ORIGINAL_FILE_EXTENSION -exec grep -a '`' {} \; > $INPUT_DIR/$outputFileName
 
   echo "Output file is ready and it starts like this:"
